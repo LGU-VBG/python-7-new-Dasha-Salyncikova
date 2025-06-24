3e4b5fbb0f61a52b435bf552189ddb57c5758eba
Dasha-Salyncikova <salyncikovad@gmail.com>
2025-06-24 10:24:29 +0300

    Update task.py

--- task.py
+++ task.py
@@ -1,1 +1,23 @@
+# a = [1, 2, 3]
+# b = [3, 4, 5]
+# c = set(a)
+# v = set(b)
+# n = c.intersection(v)
+# n = list(sorted(n))
+# print(n)
 
+
+a = set(range(52))
+
+q = set(range(23))
+
+w = set(range(35))
+
+y = set(range(16))
+
+t = set(w.update(q))
+e = (t.difference(y))
+l = (a-e)
+
+
+print(l)
